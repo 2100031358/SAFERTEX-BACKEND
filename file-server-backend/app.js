f1ef42023afbc558a313fa6a2633f3cb0425ed2d
taskeen <you@example.com>
2024-04-06 12:41:58 +0530

    commit

---
+++ file-server-backend/app.js
@@ -0,0 +1,30 @@
+// app.js
+const express = require('express');
+const multer  = require('multer');
+const fs = require('fs');
+
+const app = express();
+const port = 3000;
+
+app.use(express.static('public'));
+
+// Set up multer for file upload
+const upload = multer({ dest: 'uploads/' });
+
+// POST endpoint for file upload
+app.post('/upload', upload.single('file'), (req, res) => {
+    const fileName = req.file.originalname;
+    const fileContent = fs.readFileSync(req.file.path);
+
+    fs.writeFileSync(`public/${fileName}`, fileContent);
+
+    res.send('File uploaded successfully');
+});
+
+app.get('/', (req, res) => {
+    res.send('Hello 2100031358')
+})
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
